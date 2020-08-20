f1cc8ef72a874dfeddf8ba99ff27eb0527b8d24f
prashant kumar singh <backbencherspot@gmail.com>
2020-08-20 10:01:16 +0530

    Update raw_model.py

--- raw_model.py
+++ raw_model.py
@@ -2,11 +2,11 @@
 
 from __future__ import print_function
 import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from tenorflow.keras.datasets import mnist
+from tenorflow.keras.models import Sequential
+from tenorflow.keras.layers import Dense, Dropout, Flatten
+from tenorflow.keras.layers import Conv2D, MaxPooling2D
+from tenorflow.keras import backend as K
 
 batch_size = 128
 num_classes = 10
